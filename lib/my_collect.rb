6e6a32cb052cb6250998ae3210cdd1d697a5f570
Kari O'Neal <triadfirewood@gmail.com>
2018-08-01 19:58:40 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,14 +1,9 @@
 def my_collect(collection)
   i = 0
   new_collection = []
-  while
-  collection.each do |language| 
-    language.uppercase
-  new_collection << language
+  while i < collection.length
+  new_collection << yield (collection[i])
+  i +=1
 end
 new_collection
 end
-
- 
-  while i < collection.length 
-  new_collection << yield(collection[i])
